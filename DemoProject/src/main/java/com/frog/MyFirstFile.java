33b66fa525c79eab0970efd4988ff0a11865be00
sankar d <sankarkumard87@gmail.com>
2018-08-17 02:41:33 -0400

    changes 222

--- DemoProject/src/main/java/com/frog/MyFirstFile.java
+++ DemoProject/src/main/java/com/frog/MyFirstFile.java
@@ -4,6 +4,5 @@ public class MyFirstFile{
     public static void main(String args[]){
       System.out.println("Hello World");
       System.out.println("Update test");
-      System.out.println("dsdsds");
     }
 }
